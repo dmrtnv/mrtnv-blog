5658a9a62dbf9325fe459caeafe3dea79b563cc6
dmrtnv <dimasmart2000@gmail.com>
2023-12-14 23:08:47 +0200

    Updated header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,11 +7,21 @@ import Link from 'next/link';
 
 function Header() {
   return (
-    <div className='flex w-full items-center justify-between border-b-2 p-4'>
-      <Button variant='ghost' asChild>
+    <div className='flex w-full items-center border-b-2 p-4'>
+      <Button variant='ghost' className='mr-auto text-lg' asChild>
         <Link href='/'>Header</Link>
       </Button>
+
       <ModeToggle />
+      <div className='flex items-center gap-1'>
+        <Button variant='ghost' asChild>
+          <Link href='/login'>Log In</Link>
+        </Button>
+        /
+        <Button variant='ghost' asChild>
+          <Link href='/signup'>Sign Up</Link>
+        </Button>
+      </div>
     </div>
   );
 }
