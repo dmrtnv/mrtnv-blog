2ff4804a241f2d4d7f30691028d0da14e8bd2ada
dmrtnv <dimasmart2000@gmail.com>
2023-12-14 04:43:51 +0200

    minor

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,14 +1,11 @@
 import jwt from 'jsonwebtoken';
 
-const JWT_ACCESS_SECRET =
-  '752db3803b9425545488f9d5f3e9cea125266a87a70d2f311c6e1dc33545e390';
-const JWT_REFRESH_SECRET =
-  '1feed900d04b51e2500d475c58e301266e9a6d9c8d58e45af7283e5e0cb4dc4c';
-
 export function generateTokens(payload: any) {
-  const accessToken = jwt.sign(payload, JWT_ACCESS_SECRET, { expiresIn: '5m' });
+  const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET ?? '', {
+    expiresIn: '5m',
+  });
 
-  const refreshToken = jwt.sign(payload, JWT_REFRESH_SECRET, {
+  const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET ?? '', {
     expiresIn: '1d',
   });
 
