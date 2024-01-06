f72984e138f8636f3d900adbb9d36044ee703142
dmrtnv <dimasmart2000@gmail.com>
2024-01-06 15:20:00 +0100

    Added GET and PUT methods to /api/users/:username

--- app/api/users/[username]/route.ts
+++ app/api/users/[username]/route.ts
@@ -1,23 +1,61 @@
-import { PrismaClient } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
+import z from 'zod';
 
-const prisma = new PrismaClient();
+const UpdateFieldSchema = z.object({
+  bio: z.string().optional(),
+  profilePictureUrl: z.string().url().optional(),
+});
 
 export async function GET(request: Request) {
   const url = request.url;
   const username = url.slice(url.lastIndexOf('/') + 1);
 
   try {
-    const userExists = !!(await prisma.user.count({
+    const user = await prisma.user.findFirst({
       where: {
         username,
       },
-    }));
+      select: {
+        id: true,
+        username: true,
+        fullName: true,
+        bio: true,
+        profilePictureUrl: true,
+      },
+    });
 
-    return NextResponse.json({ userExists });
+    return NextResponse.json({ user }, { status: 200 });
   } catch (err: any) {
     return NextResponse.json({ error: err.message }, { status: 500 });
   }
+}
 
-  return NextResponse.json({ username });
+export async function PUT(req: NextRequest) {
+  const url = req.url;
+  const username = url.slice(url.lastIndexOf('/') + 1);
+
+  const data = await req.json();
+
+  try {
+    const newFields = UpdateFieldSchema.parse(data);
+
+    const user = await prisma.user.update({
+      where: { username },
+      data: {
+        ...newFields,
+      },
+      select: {
+        id: true,
+        username: true,
+        fullName: true,
+        bio: true,
+        profilePictureUrl: true,
+      },
+    });
+
+    return NextResponse.json({ user }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ err }, { status: 400 });
+  }
 }
