eeb38e341195c96b4375d8133f5366d5c028b6b0
dmrtnv <dimasmart2000@gmail.com>
2024-01-07 12:40:59 +0100

    Created CallToSignIn

---
+++ components/CallToSignIn.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import { Card, CardHeader, CardTitle } from './ui/card';
+
+function CallToSignIn() {
+  return (
+    <Card className='my-2'>
+      <CardHeader>
+        <CardTitle className='text-center'>
+          <Link href='/login' className='mr-2 text-sky-600 hover:underline'>
+            Log In
+          </Link>
+          <span>to use Next Blog!</span>
+        </CardTitle>
+      </CardHeader>
+    </Card>
+  );
+}
+
+export default CallToSignIn;
