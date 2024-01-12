4fdf0e25829ffcfef1522bbcc9e36dec3bdc46ac
dmrtnv <dimasmart2000@gmail.com>
2024-01-12 15:14:29 +0200

    Removed unused imports

--- app/(user)/profile/page.tsx
+++ app/(user)/profile/page.tsx
@@ -4,15 +4,12 @@ import { useSession } from '@/contexts/SessionProvider';
 import BioForm from '@/components/BioForm';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import React from 'react';
-import z from 'zod';
 import { Loader2 } from 'lucide-react';
 import PictureProfileForm from '@/components/PictureProfileForm';
-import { useRouter } from 'next/navigation';
 import CallToSignIn from '@/components/CallToSignIn';
 
 function ProfilePage() {
   const { user, isLoading } = useSession();
-  const router = useRouter();
 
   if (isLoading) return <Loader2 className='mx-auto my-2 animate-spin' />;
 
@@ -27,7 +24,7 @@ function ProfilePage() {
           <span className='font-semibold'>Profile Picture</span>
         </CardDescription>
       </CardHeader>
-      <CardContent>
+      <CardContent className='flex flex-col gap-4'>
         <BioForm />
 
         <PictureProfileForm />
