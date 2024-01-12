f4bbc7adae684d9c2a0332fb809886d0ba9b0d8b
dmrtnv <dimasmart2000@gmail.com>
2024-01-13 00:06:11 +0200

    Created PostPage

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,18 +1,35 @@
 'use client';
 
+import { fetchCommentsByPostId } from '@/api/comment';
 import { fetchPostById } from '@/api/posts';
+import CommentsSection from '@/components/CommentsSection';
 import Post from '@/components/Post';
-import api from '@/lib/api';
+import { Loader2 } from 'lucide-react';
 import { useQuery } from 'react-query';
 
 function PostPage({ params }: { params: { postId: string } }) {
-  const { data: post, isLoading, isError } = useQuery(['posts', params.postId], () => fetchPostById(params.postId));
+  const {
+    data: post,
+    isLoading: isPostLoading,
+    isError: isPostError,
+  } = useQuery(['posts', params.postId], () => fetchPostById(params.postId));
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error</div>;
+  const {
+    data: comments,
+    isLoading: isCommentsLoading,
+    isError: isCommentsError,
+  } = useQuery(['posts', post?.id, 'comments'], () => (post ? fetchCommentsByPostId(post?.id) : null));
+
+  if (isCommentsLoading || isPostLoading) return <Loader2 className='mx-auto my-2 animate-spin' />;
+  if (isPostError || isCommentsError) return <div>Error</div>;
   if (!post) return <div>Post does not exist</div>;
 
-  return <Post post={post} />;
+  return (
+    <>
+      <Post post={post} />
+      <CommentsSection comments={comments ?? []} />
+    </>
+  );
 }
 
 export default PostPage;
