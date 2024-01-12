735fbcf0ee87689dd130a4e9d8860e634488942b
dmrtnv <dimasmart2000@gmail.com>
2024-01-13 00:06:50 +0200

    Added /api/posts/[postId]/comments endpoint with GET method

---
+++ app/api/posts/[id]/comments/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server';
+import db from '@/lib/db';
+
+export async function GET(req: NextRequest) {
+  let url = req.nextUrl.href;
+  url = url.slice(0, url.lastIndexOf('/'));
+  const postId = url.slice(url.lastIndexOf('/') + 1);
+
+  try {
+    const comments = await db.comment.findMany({
+      where: { postId: +postId },
+      select: {
+        id: true,
+        text: true,
+        author: {
+          select: {
+            id: true,
+            fullName: true,
+            username: true,
+            bio: true,
+            profilePictureUrl: true,
+          },
+        },
+        postId: true,
+      },
+    });
+
+    return NextResponse.json(comments, { status: 200 });
+  } catch (err: unknown) {
+    return NextResponse.json(err, { status: 500 });
+  }
+}
