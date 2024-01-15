fc6bc955451040a33b3e86242f628ea79e8db9df
dmrtnv <dimasmart2000@gmail.com>
2024-01-15 14:51:42 +0200

    improved image dialog

--- components/ImageDialog.tsx
+++ components/ImageDialog.tsx
@@ -3,6 +3,7 @@ import { Dialog, DialogContent, DialogTrigger } from './ui/dialog';
 import { UserType } from '@/types/User';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import Image from 'next/image';
+import { cn } from '@/lib/utils';
 
 function ImageDialog({ user }: { user: UserType }) {
   const img = user.profilePicture;
@@ -10,20 +11,17 @@ function ImageDialog({ user }: { user: UserType }) {
 
   return (
     <Dialog>
-      <DialogTrigger>
-        <div className='shadow-l h-40 w-40 self-start overflow-hidden rounded-full object-contain'>
-          <Image
-            alt='profile picture'
-            src={img.src}
-            width={img.width}
-            height={img.height}
-            placeholder='blur'
-            blurDataURL={img.blurDataUrl}
-            quality={100}
-          />
-          {/* <AvatarImage src={user.profilePicture?.src ?? undefined} />
-          <AvatarFallback>{user.fullName.split(' ').reduce((prev, current) => prev + current[0], '')}</AvatarFallback> */}
-        </div>
+      <DialogTrigger className='h-40 w-40 overflow-hidden rounded-full'>
+        <Image
+          alt='profile picture'
+          src={img.src}
+          width={img.width}
+          height={img.height}
+          placeholder='blur'
+          blurDataURL={img.blurDataUrl}
+          quality={100}
+          className={cn('object-cover', img.width > img.height && 'h-full')}
+        />
       </DialogTrigger>
       <DialogContent className='bg-transparent'>
         <Image
@@ -34,7 +32,7 @@ function ImageDialog({ user }: { user: UserType }) {
           placeholder='blur'
           blurDataURL={img.blurDataUrl}
           quality={100}
-          className='rounded-lg'
+          className=''
         />
       </DialogContent>
     </Dialog>
