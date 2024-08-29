5fa716e1ebcc3b4df57a41395aa7fccf313c49b7
dmrtnv <dimasmart2000@gmail.com>
2024-08-29 13:11:15 +0200

    Update prisma schema

--- prisma/migrations/20240102104045_extended_user/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE `Comment` MODIFY `text` TEXT NOT NULL;
-
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `bio` VARCHAR(191) NULL,
-    ADD COLUMN `fullName` VARCHAR(191) NULL,
-    ADD COLUMN `profilePictureUrl` VARCHAR(191) NULL;
